d64c16ea2265fc834fba604e9d29869baab8d5cd
Joao Oliveira <joaofvmo1002@gmail.com>
2025-08-14 23:10:20 +0100

    feat: add do_by_end to agenda item form

--- app/javascript/controllers/agenda_item_range_selector_controller.js
+++ app/javascript/controllers/agenda_item_range_selector_controller.js
@@ -2,17 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="agenda-item-range-selector"
 export default class extends Controller {
-  static targets = [ "startTimeRow", "endTimeRow" ]
+  static targets = [ "startTimeRow", "endTimeRow", "doByStartRow", "doByEndRow" ]
 
   connect() {
     console.debug("hasStartTimeRow", this.hasStartTimeRowTarget)
     console.debug("hasEndTimeRow", this.hasEndTimeRowTarget)
+    console.debug("hasDoByStartRow", this.hasDoByStartRow)
+    console.debug("hasDoByEndRow", this.hasDoByEndRow)
 
     this.updateExecutionTimeVisibility()
+    this.updateDoTimeVisibility()
   }
 
   onStartInput(event) {
     this.updateExecutionTimeVisibility()
+  }
+
+  onDoByStartInput(event) {
+    this.updateDoTimeVisibility()
   }
 
   updateExecutionTimeVisibility() {
@@ -20,5 +27,12 @@ export default class extends Controller {
       this.endTimeRowTarget.hidden = true
     else
       this.endTimeRowTarget.hidden = false
+  }
+
+  updateDoTimeVisibility() {
+    if (this.doByStartRowTarget.querySelector("input").value.length == 0)
+      this.doByEndRowTarget.hidden = true
+    else
+      this.doByEndRowTarget.hidden = false
   }
 }
